128d514c9f55f683018a816c5faf147202c9c39e
Rex <rex@rex.gs>
2018-04-19 17:25:04 +0900

    returning last submission dates with GetScoreboard

--- adctf/handlers/scoreboard.go
+++ adctf/handlers/scoreboard.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"sort"
+	"time"
 
 	"github.com/activedefense/submarine/rules"
 	"github.com/labstack/echo"
@@ -10,10 +11,11 @@ import (
 
 func GetScoreboard(c echo.Context) error {
 	type record struct {
-		Order int    `json:"order"`
-		ID    int    `json:"id"`
-		Name  string `json:"name"`
-		Score int    `json:"score"`
+		Order          int       `json:"order"`
+		ID             int       `json:"id"`
+		Name           string    `json:"name"`
+		Score          int       `json:"score"`
+		LastSubmission time.Time `json:"last"`
 	}
 
 	j, _ := c.Get("jeopardy").(rules.JeopardyRule)
@@ -24,10 +26,11 @@ func GetScoreboard(c echo.Context) error {
 	result := make([]record, len(scores))
 	for i, item := range scores {
 		result[i] = record{
-			Order: i + 1,
-			ID:    item.GetTeam().GetID(),
-			Name:  item.GetTeam().GetName(),
-			Score: item.GetScore(),
+			Order:          i + 1,
+			ID:             item.GetTeam().GetID(),
+			Name:           item.GetTeam().GetName(),
+			Score:          item.GetScore(),
+			LastSubmission: item.GetLastSubmission(),
 		}
 	}
 
