057aefeb1dd89897004ec3d8ad0a57884a46ac7a
Rex <rex@rex.gs>
2018-04-22 16:39:01 +0900

    read configurations from args and envs

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"flag"
+	"os"
+	"strings"
+
 	"github.com/activedefense/submarine/adctf"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	server := adctf.New(adctf.ADCTFConfig{
-		DriverName:     "sqlite3",
-		DataSourceName: ":memory:?parseTime=true",
-		JWTSecret:      []byte("e81061ace8c9e0b568c00075ecda0d8c42d"),
-		Debug:          true,
+	var driver string
+	var source string
+	var secret string
+	var listen string
+	var debug bool
+	flag.StringVar(&driver, "driver", "sqlite3", "DB Driver Name")
+	flag.StringVar(&source, "source", "submarine.db?parseTime=true", "DB Source Name")
+	flag.StringVar(&secret, "secret", "e81061ace8c9e0b568c00075ecda0d8c42d", "Application SecretKey")
+	flag.BoolVar(&debug, "debug", false, "Debug mode")
+	flag.StringVar(&listen, "listen", "127.0.0.1:8000", "Host/port to listen")
+	flag.VisitAll(func(f *flag.Flag) {
+		if s := os.Getenv(strings.ToUpper("submarine_" + f.Name)); s != "" {
+			f.Value.Set(s)
+		}
 	})
-	server.Start("127.0.0.1:8000")
+	flag.Parse()
+
+	server := adctf.New(adctf.ADCTFConfig{
+		DriverName:     driver,
+		DataSourceName: source,
+		JWTSecret:      []byte(secret),
+		Debug:          debug,
+	})
+	server.Start(listen)
 }
